d3638311b39d0fe6b93dde1ca994ea78251af870
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-11-21 14:25:42 +0100

    Installing react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+ const [email, setEmail] = useState('');
+ const [password, setPassword] = useState('');
+
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('A login was submitted:', { email, password });
+ };
+
+ return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+ );
+}
+
+export default App;
