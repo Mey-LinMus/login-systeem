c68532a2e1ae45d76cbf495c5bc3768707c12c15
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-11-24 14:23:06 +0100

    Adding some routes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../styles/forum.css";
 
-const Login = () => {
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      alert(data.message);
+    } catch (error) {
+      console.error("Error logging in", error);
+      alert(`Error: ${error.message}`);
+    }
+  };
+
   return (
-    <div>
-      
+    <div className="App">
+      <h2>User Login</h2>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <br />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <br />
+
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
